1c2ef4e59d46ffaa470c958e99bcea0465eed10c
Marin Vasilev <mvasilev@axway.com>
2016-12-01 12:54:45 +0200

    Question model update

--- models/question.js
+++ models/question.js
@@ -3,12 +3,12 @@ var Arrow = require('arrow');
 // TODO: Rename "yourModel" (and this file) to whatever you want.
 module.exports = Arrow.Model.extend('question', {
 	fields: {
-		question_id: {type: String, required: true},
-		owner: {type: Object, required: true},
-		title: {type: String, required: true},
-		body_markdown: {type: String, required: true},
-		answers: {type: Array},
-		tags: {type: Array},
-
+		question_id: { type: String, required: true },
+		owner: { type: Object, required: true },
+		title: { type: String, required: true },
+		body_markdown: { type: String, required: true },
+		answers: { type: Array },
+		tags: { type: Array },
+		link: { type: String },
 	}
 });
